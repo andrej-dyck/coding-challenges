b4c0b5a8fb4963596aa06deda8d09614439a6fcc
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2021-02-21 19:20:38 +0100

    Add gradle kotlin project

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.30"
+}
+
+group = "ad.kata"
+version = "1.0"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.18.1")
+}
+
+/* Compile to JVM 8 */
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+/* Source sets by Kotlin conventions /src and /test */
+sourceSets.getByName("main") {
+    withConvention(KotlinSourceSet::class) {
+        kotlin.srcDirs("src/")
+    }
+}
+
+sourceSets.getByName("test") {
+    withConvention(KotlinSourceSet::class) {
+        kotlin.srcDirs("test/")
+    }
+}
+
+/* Check with Junit 5 only */
+tasks.test {
+    useJUnitPlatform {
+        excludeEngines("junit-vintage")
+    }
+}
+
+/* Gradle Wrapper */
+tasks.withType<Wrapper> {
+    gradleVersion = "6.8"
+    distributionType = Wrapper.DistributionType.BIN
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "ad.kata.coding-challenges"
+
